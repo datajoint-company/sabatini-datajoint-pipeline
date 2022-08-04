3d8f242c11bad23a43894c991a5693ec06f80bfc
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2022-08-04 18:46:43 -0500

    docker folder reoganization, added support pipeline back

---
+++ workflow/support/dlc_support.py
@@ -0,0 +1,22 @@
+from workflow import db_prefix
+from workflow.pipeline import dlc
+
+from .file_manifest import FileManifest, support_db_prefix
+
+__all__ = ["dlc_model_support"]
+
+# ---- support pipeline for facemap ----
+from element_deeplabcut_support import dlc_model_support
+
+# rework key_source
+model = dlc.model
+dlc_model_support.activate(
+    support_db_prefix + "dlc_model_support", linking_module=__name__
+)
+
+dlc.model.RecordingInfo.key_source = (
+    dlc.model.RecordingInfo.key_source & dlc_model_support.PreRecordingInfo
+)
+dlc.model.PoseEstimation.key_source = (
+    dlc.model.PoseEstimation.key_source & dlc_model_support.PrePoseEstimation
+)
