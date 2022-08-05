6768740c211618854e9fa78bff8e99e17c7c9545
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2022-08-05 15:48:01 -0500

    support modifications

--- workflow/populate/worker.py
+++ workflow/populate/worker.py
@@ -2,11 +2,13 @@ import datajoint as dj
 from datajoint_utilities.dj_worker import DataJointWorker, WorkerLog, ErrorLog
 from workflow import db_prefix
 from workflow.pipeline import session, ephys, scan, imaging, model as dlc_model, train as dlc_train
+from workflow.support import ephys_support, imaging_support, facemap_support, dlc_model_support
+from .ingest_tasks import generate_processing_task
 
 logger = dj.logger
 
 __all__ = ["standard_worker", "dlc_worker", "calcium_imaging_worker", \
-     "spike_sorting_worker", "WorkerLog", "ErrorLog"]
+     "spike_sorting_worker", "WorkerLog",  "ErrorLog"]
 
 def auto_generate_probe_insertions():
     for skey in (session.Session - ephys.ProbeInsertion).fetch('KEY'):
@@ -26,6 +28,24 @@ def auto_generate_clustering_tasks():
             logger.error(str(error))
             ErrorLog.log_exception(rkey, ephys.ClusteringTask.auto_generate_entries, error)
 
+def auto_generate_processing_tasks():
+    for scan_key in (scan.ScanInfo - imaging.ProcessingTask).fetch("KEY"):
+        try:
+            logger.debug(
+                f"Making {scan_key} -> {imaging.ProcessingTask.full_table_name}"
+            )
+            generate_processing_task(scan_key)
+        except Exception as error:
+            logger.debug(
+                f"Error making {scan_key} -> {imaging.ProcessingTask.full_table_name} - {str(error)}"
+            )
+            ErrorLog.log_exception(scan_key, generate_processing_task, error)
+        else:
+            logger.debug(
+                f"Success making {scan_key} -> {imaging.ProcessingTask.full_table_name}"
+            )
+
+
 # -------- Define process(s) --------
 org_name, workflow_name, _ = db_prefix.split("_")
 
@@ -41,18 +61,27 @@ standard_worker = DataJointWorker('standard_worker',
                                   sleep_duration=30,
                                   autoclear_error_patterns=autoclear_error_patterns)
 
+standard_worker(ephys_support.PreProbeInsertion)
 standard_worker(auto_generate_probe_insertions)
+standard_worker(ephys_support.PreProbeInsertion.clean_up)
 
-standard_worker(ephys.EphysRecording, max_calls=5)
+standard_worker(ephys_support.PreEphysRecording)
+standard_worker(ephys.EphysRecording, max_calls=10)
+standard_worker(ephys_support.PreEphysRecording.clean_up)
 
 standard_worker(auto_generate_clustering_tasks)
 
-standard_worker(ephys.CuratedClustering, max_calls=5)
+standard_worker(ephys_support.PreCuratedClustering)
+standard_worker(ephys.CuratedClustering, max_calls=10)
+standard_worker(ephys_support.PreCuratedClustering.clean_up)
 
-standard_worker(ephys.WaveformSet, max_calls=5)
+standard_worker(ephys_support.PreWaveformSet)
+standard_worker(ephys.WaveformSet, max_calls=1)
+standard_worker(ephys_support.PreWaveformSet.clean_up)
 
-standard_worker(ephys.LFP, max_calls=5)
-
+standard_worker(ephys_support.PreLFP)
+standard_worker(ephys.LFP, max_calls=1)
+standard_worker(ephys_support.PreLFP.clean_up)
 
 # spike_sorting process for GPU required jobs
 spike_sorting_worker = DataJointWorker('spike_sorting_worker',
@@ -64,18 +93,32 @@ spike_sorting_worker = DataJointWorker('spike_sorting_worker',
 
 spike_sorting_worker(ephys.Clustering, max_calls=6)
 
-# imaging
+# Imaging analyses run by standard worker
+
+standard_worker(imaging_support.PreScanInfo, max_calls=1)
 standard_worker(scan.ScanInfo, max_calls=5)
+standard_worker(imaging_support.PreScanInfo.clean_up)
 
+standard_worker(auto_generate_processing_tasks)
+
+standard_worker(imaging_support.PreMotionCorrection, max_calls=2)
 standard_worker(imaging.MotionCorrection, max_calls=5)
+standard_worker(imaging_support.PreMotionCorrection.clean_up)
 
+standard_worker(imaging_support.PreSegmentation, max_calls=2)
 standard_worker(imaging.Segmentation, max_calls=5)
+standard_worker(imaging_support.PreSegmentation.clean_up)
 
+standard_worker(imaging_support.PreFluorescence, max_calls=2)
 standard_worker(imaging.Fluorescence, max_calls=5)
+standard_worker(imaging_support.PreFluorescence.clean_up)
 
+standard_worker(imaging_support.PreActivity, max_calls=3)
 standard_worker(imaging.Activity, max_calls=5)
+standard_worker(imaging_support.PreActivity.clean_up)
 
-# analysis worker
+
+# calcum imaging worker
 calcium_imaging_worker = DataJointWorker(
     "calcium_imaging_worker",
     worker_schema_name,
