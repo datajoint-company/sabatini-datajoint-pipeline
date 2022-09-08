1250b88007f34c1b705455b9c9e21dbf5cbccb95
Ubuntu <ubuntu@ip-172-20-48-232.us-east-2.compute.internal>
2022-09-08 18:41:22 +0000

    Fix worker to include support for clusteringtables

--- workflow/populate/worker.py
+++ workflow/populate/worker.py
@@ -62,6 +62,8 @@ standard_worker(ephys_support.PreCuratedClustering)
 standard_worker(ephys.CuratedClustering, max_calls=10)
 standard_worker(ephys_support.PreCuratedClustering.clean_up)
 
+
+
 standard_worker(ephys_support.PreWaveformSet)
 standard_worker(ephys.WaveformSet, max_calls=1)
 standard_worker(ephys_support.PreWaveformSet.clean_up)
@@ -78,7 +80,10 @@ spike_sorting_worker = DataJointWorker('spike_sorting_worker',
                                        sleep_duration=30,
                                        autoclear_error_patterns=autoclear_error_patterns)
 
+spike_sorting_worker(ephys_support.PreClustering)
 spike_sorting_worker(ephys.Clustering, max_calls=6)
+spike_sorting_worker(ephys_support.PostClustering)
+spike_sorting_worker(ephys_support.PreClustering.clean_up)
 
 # Imaging analyses run by standard worker
 
